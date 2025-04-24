f1ed0ea9ca52a1294a461cdc3bc88b9c592104f9
Sathvik Kadimisetty <66404509+Jellal-17@users.noreply.github.com>
2025-04-24 11:20:13 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,3 +1,25 @@
+# MIT License
+
+# Copyright (c) 2025 Sathvik Kadimisetty
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # State can be defined as following
 # 1. Positions of each agent: [(x1, y1), (x2, y2), (x3, y3)]
 # 2. Colors acquired: track for each agent whether it has reached its target color yet (True/False).
